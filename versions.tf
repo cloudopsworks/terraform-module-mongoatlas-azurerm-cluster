32e4c9a9ba9c069a082a04f31892234c5461a99a
Cristian Beraha <cristian@cloudops.works>
2026-04-10 16:17:12 -0300

    fix: update mongodbatlas provider to ~> 2.1
    +semver: feature

--- versions.tf
+++ versions.tf
@@ -16,7 +16,7 @@ terraform {
     }
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
-      version = "~> 1.32"
+      version = "~> 2.1"
     }
     time = {
       source  = "hashicorp/time"
